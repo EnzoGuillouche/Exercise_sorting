fbdda2d803eea0d77a7e4ac43f28b9f0a15dbd44
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-01-10 17:40:00 +0100

    Added required stuff

--- Exercise_sorting/start.sh
+++ Exercise_sorting/start.sh
@@ -1,2 +1,14 @@
-g++ -o bin/Exercise_sorting main.cpp -std=c++17
-./bin/Exercise_sorting
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS specific commands
+    echo "Running on macOS"
+    g++ -o bin/Exercise_sorting main.cpp -std=c++17
+    ./bin/Exercise_sorting
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    # Windows specific commands (Cygwin or MinGW)
+    echo "Running on Windows"
+    g++ -o bin/Exercise_sorting.exe main.cpp -std=c++17
+    ./bin/Exercise_sorting.exe
+else
+    echo "Unknown OS type: $OSTYPE"
+    exit 1
+fi
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Exercise_sorting
-A repository for sorting out teams of students.
+
+This repository stands for sorting out teams of students. This is an exercise to deal with C++ and datastructures.
+
+## How does it work?
+
+The main program contains 8 teams. At the beginning, no students are sort out in the team yet. Once the user sorts the students out randomly, they are assigned among the teams.
+
+You just have to run `start.sh` to run the program. It works on both Windows and macOS.
